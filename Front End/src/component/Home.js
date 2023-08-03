bdb8315611db9e8121acfad9578a069120816a31
Pumudu Nadun Rajakaruna <115540884+PumuduRajakaruna@users.noreply.github.com>
2023-08-03 23:57:04 +0530

    Add files via upload

---
+++ Front End/src/component/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Appbar from '../component/appbar';
+import '../component/Home.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Appbar />
+      <h1 className='header'>WELCOME TO THE STUDENT REGISTRATION PLATFORM</h1>
+    </div>
+  )
+}
