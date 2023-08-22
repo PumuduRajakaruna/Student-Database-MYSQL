972df3882e334c41d3e8489ac23d45f462912a65
PumuduRajakaruna <e19306@eng.pdn.ac.lk>
2023-08-22 16:27:30 +0530

    commit

--- Front End/src/component/Home.js
+++ Front End/src/component/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Appbar from '../component/appbar';
 import '../component/Home.css'
+import Login from './Login';
 
 export default function Home() {
   return (
     <div>
-      <Appbar />
+      <Login />
       <h1 className='header'>WELCOME TO THE STUDENT REGISTRATION PLATFORM</h1>
     </div>
   )
